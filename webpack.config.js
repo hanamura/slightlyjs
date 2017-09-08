24426f940e446c33e94b035efe2cd572702aeb9d
Taro Hanamura <m@hanamurataro.com>
2017-09-09 02:31:04 +0900

    Add package.json and Babel/Webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './src/slightly.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  }
+}
