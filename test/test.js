22b2a9a5af7df66ea5bb68593a1f3b0dea9d7dbc
Taro Hanamura <m@hanamurataro.com>
2017-09-10 15:08:41 +0900

    Throw error for testing

--- test/test.js
+++ test/test.js
@@ -2,5 +2,6 @@ describe('hello', () => {
   it('world', () => {
     document.body.innerHTML = '<div>Hello World</div>'
     console.log(document)
+    throw new Error('Hello World Error')
   })
 })
